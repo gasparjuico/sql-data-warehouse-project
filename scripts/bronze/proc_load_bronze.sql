02fb44443c6a2f707aedfa338513cd558160b90e
gasparjuico <157286423+gasparjuico@users.noreply.github.com>
2026-02-08 21:50:12 +0800

    Create proc_load_bronze.sql

--- scripts/bronze/proc_load_bronze.sql
+++ scripts/bronze/proc_load_bronze.sql
@@ -41,7 +41,7 @@ BEGIN
 
 		PRINT '>> Inserting Data Into: bronze.crm_cust_info';
 		BULK INSERT bronze.crm_cust_info
-		FROM 'C:\Users\Gaspar\Desktop\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
+		FROM 'C:\Path\To\Your\File\cust_info.csv' -- Change this to your local file path
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -57,7 +57,7 @@ BEGIN
 
 		PRINT '>> Inserting Data Into: bronze.crm_prd_info';
 		BULK INSERT bronze.crm_prd_info
-		FROM 'C:\Users\Gaspar\Desktop\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
+		FROM 'C:\Path\To\Your\File\prd_info.csv' -- Change this to your local file path
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -73,7 +73,7 @@ BEGIN
 
 		PRINT '>> Inserting Data Into: bronze.crm_sales_details';
 		BULK INSERT bronze.crm_sales_details
-		FROM 'C:\Users\Gaspar\Desktop\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
+		FROM 'C:\Path\To\Your\File\sales_details.csv' -- Change this to your local file path
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -93,7 +93,7 @@ BEGIN
 
 		PRINT '>> Inserting Data Into: bronze.erp_cust_az12';
 		BULK INSERT bronze.erp_cust_az12
-		FROM 'C:\Users\Gaspar\Desktop\sql-data-warehouse-project\datasets\source_erp\CUST_AZ12.csv'
+		FROM 'C:\Path\To\Your\File\CUST_AZ12.csv' -- Change this to your local file path
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -109,7 +109,7 @@ BEGIN
 
 		PRINT '>> Inserting Data Into: bronze.erp_loc_a101';
 		BULK INSERT bronze.erp_loc_a101
-		FROM 'C:\Users\Gaspar\Desktop\sql-data-warehouse-project\datasets\source_erp\LOC_A101.csv'
+		FROM 'C:\Path\To\Your\File\LOC_A101.csv' -- Change this to your local file path
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -125,7 +125,7 @@ BEGIN
 
 		PRINT '>> Inserting Data Into: bronze.erp_px_cat_g1v2';
 		BULK INSERT bronze.erp_px_cat_g1v2
-		FROM 'C:\Users\Gaspar\Desktop\sql-data-warehouse-project\datasets\source_erp\PX_CAT_G1V2.csv'
+		FROM 'C:\Path\To\Your\File\PX_CAT_G1V2.csv' -- Change this to your local file path
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
